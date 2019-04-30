7444512ecbd762d2c2131ec101d15e461c46a4cf
Kvartti <a1704516@myy.haaga-helia.fi>
2019-04-30 18:06:20 +0300

    flaskapp edit

--- flaskapp.py
+++ flaskapp.py
@@ -134,8 +134,7 @@ def get_countryidyear(country, year):
 
     return jsonify({'country': countries, 'emissions': emissions, 'years': years})
 
-
- if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
